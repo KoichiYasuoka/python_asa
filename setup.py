495d0784c9a31ee68c98423654c54d256afaf556
Koichi Yasuoka <yasuoka@kanji.zinbun.kyoto-u.ac.jp>
2020-08-13 14:00:13 +0900

    for full package install

--- setup.py
+++ setup.py
@@ -1,9 +1,4 @@
-from setuptools import setup, find_packages
-from os import path
-# here = path.abspath(path.dirname(__file__))
-# Get the long description from the README file
-#with open(path.join(here, 'README.md'), encoding='utf-8') as f:
-#    long_description = f.read()
+from setuptools import setup
 
 setup(
     name='asapy',  # Required
@@ -22,4 +17,5 @@ setup(
     ],
 
     packages=['asapy'],  # Required
+    package_data={"asapy":["*/*.*","*/*/*.*"]},
 )
