efa99957225c554d3939c94ad8242bb6d0862748
Koichi Yasuoka <yasuoka@kanji.zinbun.kyoto-u.ac.jp>
2020-08-14 14:44:08 +0900

    pos_change() introduced

--- asapy/parse/analyzer/Analyzer.py
+++ asapy/parse/analyzer/Analyzer.py
@@ -14,11 +14,11 @@ class Analyzer():
             import spacy
             from ginza.command_line import analyze_cabocha
             self.analyzer = type('',(object,),{'parse':spacy.load('ja_ginza')})
-            self.to_list = lambda tree: [t.replace("\t動詞,一般,", "\t動詞,自立,") for t in analyze_cabocha(tree)]
+            self.to_list = lambda tree: self.pos_change(analyze_cabocha(tree))
         else:
             import unidic2ud.cabocha as CaboCha
             self.analyzer = CaboCha.Parser(analyzertype)
-            self.to_list = lambda tree: tree.toString(CaboCha.FORMAT_LATTICE).replace("\t動詞,一般,", "\t動詞,自立,").split("\n")
+            self.to_list = lambda tree: self.pos_change(tree.toString(CaboCha.FORMAT_LATTICE).split("\n"))
 
     def parse(self, line: str) -> Result:
         m_id = 0
@@ -35,3 +35,24 @@ class Analyzer():
                 result.chunks[-1].addMorph(Morph(m_id, line))
                 m_id += 1
         return result
+
+    def pos_change(self, tree):
+        r = []
+        for t in tree:
+            d = t.split("\t")
+            if len(d) == 1:
+                m = d[0].split()
+                if len(m) > 3:
+                    if m[0] == "*":
+                        h = int(m[3].split("/")[0])
+                        i = 0
+            elif len(d) > 1:
+                m = d[1].split(",")
+                if m[0] == "動詞":
+                    m[1] = "自立" if i == h else "非自立"
+                elif m[0] == "助動詞":
+                    m[4] = "判定詞" if m[6] in {"だ", "です"} else m[4]
+                d[1] = ",".join(m)
+                i += 1
+            r.append("\t".join(d))
+        return r
