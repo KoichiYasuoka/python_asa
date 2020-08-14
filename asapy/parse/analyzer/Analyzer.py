c71a2a3b1c8bcd5a1fed0f00deed0e8428f407d8
Koichi Yasuoka <yasuoka@kanji.zinbun.kyoto-u.ac.jp>
2020-08-14 11:56:28 +0900

    GiNZA and UniDic2UD support

--- asapy/parse/analyzer/Analyzer.py
+++ asapy/parse/analyzer/Analyzer.py
@@ -1,4 +1,3 @@
-import CaboCha
 from asapy.result.Result import Result
 from asapy.result.Chunk import Chunk
 from asapy.result.Morph import Morph
@@ -8,13 +7,24 @@ class Analyzer():
 
     def __init__(self, analyzertype: str, code: str) -> None:
         if analyzertype == 'cabocha':
+            import CaboCha
             self.analyzer = CaboCha.Parser("-f1 -n1")
+            self.to_list = lambda tree: tree.toString(CaboCha.FORMAT_LATTICE).split("\n")
+        elif analyzertype == 'ginza':
+            import spacy
+            from ginza.command_line import analyze_cabocha
+            self.analyzer = type('',(object,),{'parse':spacy.load('ja_ginza')})
+            self.to_list = lambda tree: [t.replace("\t動詞,一般,", "\t動詞,自立,") for t in analyze_cabocha(tree)]
+        else:
+            import unidic2ud.cabocha as CaboCha
+            self.analyzer = CaboCha.Parser(analyzertype)
+            self.to_list = lambda tree: tree.toString(CaboCha.FORMAT_LATTICE).replace("\t動詞,一般,", "\t動詞,自立,").split("\n")
 
     def parse(self, line: str) -> Result:
         m_id = 0
         result = Result(line)
         tree = self.analyzer.parse(line)
-        line_list = tree.toString(CaboCha.FORMAT_LATTICE).split("\n")
+        line_list = self.to_list(tree)
         for line in line_list:
             if line == "EOS":
                 break
